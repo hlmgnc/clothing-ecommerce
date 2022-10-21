fc7b4eaa24c7788cee24504010665f7f667f3f97
hlmgnc <61838067+hlmgnc@users.noreply.github.com>
2022-10-21 11:08:47 +0200

    Create components for ecommerce project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Directory from "./components/directory/directory.component";
 
-function App() {
+const App = () => {
+  const categories = [
+    { title: "Hats", id: "1", imageUrl: "https://i.ibb.co/cvpntL1/hats.png" },
+    {
+      title: "Jackets",
+      id: "2",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      title: "Sneakers",
+      id: "3",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      title: "Womens",
+      id: "4",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+      size: "large",
+    },
+    { title: "Mens", id: "5", imageUrl: "https://i.ibb.co/R70vBrQ/men.png" },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Directory categories={categories}/>
   );
-}
+};
 
 export default App;
